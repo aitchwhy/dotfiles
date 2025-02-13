f8a6cf8a017b11478ee11f11df25d9a9dcf9a629
Hank Lee <hank.lee.qed@gmail.com>
2025-02-13 16:57:58 -0500

    update nvim lazy

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,53 +1,38 @@
 -- ui.lua: UI enhancements (theme, statusline, bufferline, etc.) and tools like Telescope and NvimTree
 return {
+
+  -- lua/plugins/ui.lua
+  -- Example UI plugins: colorscheme + file explorer
+
   -- COLORSCHEME
   {
     "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000, -- load immediately on startup&#8203;:contentReference[oaicite:55]{index=55}
-    config = function()
-      vim.cmd("colorscheme tokyonight-storm") -- set colorscheme (using "storm" variant here)
-    end,
-  },
-  -- zenburn
-  {
-    "daler/zenburn.nvim",
-
-    -- lazy.nvim recommends using these settings for colorschemes so they load
-    -- quickly
-    lazy = false,
-    priority = 1000,
-  },
-  -- gruvbox is an example of an alternative colorscheme, here disabled
-  {
-    "morhetz/gruvbox",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
 
-  -- onedark is another colorscheme, here enabled as a fallback for terminals
-  -- with no true-color support like the macOS Terminal.app.
-  {
-    "joshdick/onedark.vim",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- STATUSLINE
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = { theme = "tokyonight", globalstatus = true },
-        sections = {
-          lualine_c = { { "filename", path = 1 } }, -- show relative path
-          lualine_x = { { "diagnostics", sources = { "nvim_diagnostic" } }, "encoding", "filetype" },
+    dependencies = {
+      "linrongbin16/lsp-progress.nvim",
+    },
+    opts = {
+      options = { theme = "zenburn" },
+      sections = {
+        lualine_c = { { "filename", path = 2 } },
+        -- use lsp-progress plugin to show LSP activity
+        lualine_x = {
+          function()
+            return require("lsp-progress").progress()
+          end,
         },
-      })
-    end,
+      },
+    },
   },
 
   -- BUFFERLINE (tabs for buffers)
@@ -97,27 +82,6 @@ return {
     end,
   },
 
-  -- FUZZY FINDER
-  {
-    "nvim-telescope/telescope.nvim",
-    cmd = "Telescope",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          prompt_prefix = "🔍 ",
-          selection_caret = " ",
-          path_display = { "smart" },
-          file_ignore_patterns = { "node_modules", ".git/" },
-        },
-      })
-      require("telescope").load_extension("fzf") -- enable fzf-native for faster search
-    end,
-  },
-
   -- GIT SIGNS
   {
     "lewis6991/gitsigns.nvim",
@@ -133,21 +97,6 @@ return {
         },
         current_line_blame = true, -- Toggle with :Gitsigns toggle_current_line_blame
       })
-    end,
-  },
-
-  -- FUGITIVE (Git commands)
-  { "tpope/vim-fugitive", cmd = { "Git", "Gdiffsplit", "Gblame", "Glog", "Gpush", "Gpull" } },
-
-  -- DRESSING (improved vim.ui)
-  { "stevearc/dressing.nvim", event = "VeryLazy", config = true },
-
-  -- NOTIFY (better notifications)
-  {
-    "rcarriga/nvim-notify",
-    event = "VeryLazy",
-    config = function()
-      vim.notify = require("notify")
     end,
   },
 }
