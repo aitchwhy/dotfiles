0ac323dc11760317c3d3ed511f9eb2bfa0fe376f
Hank Lee <hank.lee.qed@gmail.com>
2025-02-14 17:39:07 -0500

    working nvim lazyvim plugins

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -3,74 +3,18 @@ return {
         "LazyVim/LazyVim",
         opts = {
             colorscheme = "catppuccin",
-        },
-        defaults = {
-            autocmds = true,
-            keymaps = true,
-        },
+        }
     },
 
     -- nvim-web-devicons
-    { "nvim-tree/nvim-web-devicons", opts = {} },
+    { "nvim-tree/nvim-web-devicons",                    opts = {} },
 
     -- mini.icons (standalone)
-    { "echasnovski/mini.icons", version = false },
+    { "echasnovski/mini.icons",                         version = false },
 
-    -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-    { import = "lazyvim.plugins.extras.lang.json" },
 
-    -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-    -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-    { import = "lazyvim.plugins.extras.lang.typescript" },
 
-    -- add more treesitter parsers
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "bash",
-                "c",
-                "diff",
-                "go",
-                "gomod",
-                "html",
-                "javascript",
-                "jsdoc",
-                "json",
-                "jsonc",
-                "lua",
-                "luadoc",
-                "luap",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "ruby",
-                "rust",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-            },
-        },
-    },
 
-    -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-    -- would overwrite `ensure_installed` with the new value.
-    -- If you'd rather extend the default config, use the code below instead:
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            -- add tsx and treesitter
-            vim.list_extend(opts.ensure_installed, {
-                "tsx",
-                "typescript",
-            })
-        end,
-    },
 
     -- use mini.starter instead of alpha
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
@@ -90,4 +34,5 @@ return {
             },
         },
     },
+
 }
--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -2,25 +2,40 @@ return {
     -- Modern linting
     {
         "mfussenegger/nvim-lint",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local lint = require("lint")
-            lint.linters_by_ft = {
-                python = { "ruff" },
-                javascript = { "eslint_d" },
-                typescript = { "eslint_d" },
-                javascriptreact = { "eslint_d" },
-                typescriptreact = { "eslint_d" },
-                lua = { "selene" },
-                markdown = { "markdownlint" },
-                yaml = { "actionlint" },
-                dockerfile = { "hadolint" },
-            }
-            vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-                callback = function()
-                    require("lint").try_lint()
-                end,
-            })
-        end,
+        -- Event to trigger linters
+        events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+        linters_by_ft = {
+            fish = { "fish" },
+            -- Use the "*" filetype to run linters on all filetypes.
+            -- ['*'] = { 'global linter' },
+            python = { "ruff" },
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            javascriptreact = { "eslint_d" },
+            typescriptreact = { "eslint_d" },
+            lua = { "selene" },
+            markdown = { "markdownlint" },
+            yaml = { "actionlint" },
+            dockerfile = { "hadolint" },
+
+            -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+            -- ['_'] = { 'fallback linter' },
+            -- ["*"] = { "typos" },
+        },
+
+        -- LazyVim extension to easily override linter options
+        -- or add custom linters.
+        ---@type table<string,table>
+        linters = {
+            -- -- Example of using selene only when a selene.toml file is present
+            -- selene = {
+            --   -- `condition` is another LazyVim extension that allows you to
+            --   -- dynamically enable/disable linters based on the context.
+            --   condition = function(ctx)
+            --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
+            --   end,
+            -- },
+
+        }
     },
 }
