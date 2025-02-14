210be3b9aed1796f645daa3e19792b473b54544b
Hank Lee <hank.lee.qed@gmail.com>
2025-02-14 17:39:07 -0500

    working nvim lazyvim plugins

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
+            { out, "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -18,8 +18,10 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- -- add LazyVim and import its plugins
-        { "LazyVim/LazyVim",                                import = "lazyvim.plugins" },
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
+        { import = "lazyvim.plugins.extras.linting.eslint" },
+        { import = "lazyvim.plugins.extras.formatting.prettier" },
         -- import/override with your plugins
         -- { import = "plugins" },
     },
@@ -36,7 +38,7 @@ require("lazy").setup({
     checker = {
         enabled = true, -- check for plugin updates periodically
         notify = false, -- notify on update
-    },                  -- automatically check for plugin updates
+    }, -- automatically check for plugin updates
     performance = {
         rtp = {
             -- disable some rtp plugins
--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,107 +1,19 @@
 return {
-    -- Mason LSP configuration
     {
-        "williamboman/mason-lspconfig.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "neovim/nvim-lspconfig",
-        },
-        config = function()
-            local mason_lspconfig = require("mason-lspconfig")
-            local lspconfig = require("lspconfig")
-
-            mason_lspconfig.setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-                    })
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = { eslint = {} },
+            setup = {
+                eslint = function()
+                    require("lazyvim.util").lsp.on_attach(function(client)
+                        if client.name == "eslint" then
+                            client.server_capabilities.documentFormattingProvider = true
+                        elseif client.name == "tsserver" then
+                            client.server_capabilities.documentFormattingProvider = false
+                        end
+                    end)
                 end,
-            })
-        end,
+            },
+        },
     },
 }
---
--- return {
---   -- add pyright to lspconfig
---   {
---     "neovim/nvim-lspconfig",
---     dependencies = { 'saghen/blink.cmp' },
---     ---@class PluginLspOpts
---     opts = {
---       ---@type lspconfig.options
---       ---@  -- make sure mason installs the server
---
---       servers = {
---         -- pyright will be automatically installed with mason and loaded with lspconfig
---         pyright = {},
---         lua_ls = {},
---         --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
---         --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
---         tsserver = {
---           enabled = false,
---         },
---         ts_ls = {
---           enabled = false,
---         },
---         vtsls = {},
---         tailwindcss = {},
---         html = {},
---         cssls = {},
---         jsonls = {},
---         clangd = {},
---         gopls = {},
---         pylyzer = {},
---         ruff_lsp = {},
---         ruff_d = {},
---         -- Add other servers here
---         eslint = {
---           settings = {
---             -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
---             workingDirectories = { mode = "auto" },
---             format = auto_format,
---           },
---         },
---       },
---     },
---   },
---   setup = {
---     eslint = function()
---       if not auto_format then
---         return
---       end
---
---       local function get_client(buf)
---         return LazyVim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
---       end
---
---       local formatter = LazyVim.lsp.formatter({
---         name = "eslint: lsp",
---         primary = false,
---         priority = 200,
---         filter = "eslint",
---       })
---
---       -- Use EslintFixAll on Neovim < 0.10.0
---       if not pcall(require, "vim.lsp._dynamic") then
---         formatter.name = "eslint: EslintFixAll"
---         formatter.sources = function(buf)
---           local client = get_client(buf)
---           return client and { "eslint" } or {}
---         end
---         formatter.format = function(buf)
---           local client = get_client(buf)
---           if client then
---             local diag = vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
---             if #diag > 0 then
---               vim.cmd("EslintFixAll")
---             end
---           end
---         end
---       end
---
---       -- register the formatter with LazyVim
---       LazyVim.format.register(formatter)
---     end,
---   },
--- }
