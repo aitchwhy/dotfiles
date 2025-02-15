f8e95561dcdca733f2d25f92c54c09ac2fb07ca8
Hank Lee <hank.lee.qed@gmail.com>
2025-02-14 20:20:30 -0500

    merge nvim configs]

---
+++ config/nvim/lua/plugins/symbols.lua
@@ -0,0 +1,8 @@
+return {
+    {
+      "simrat39/symbols-outline.nvim",
+      cmd = "SymbolsOutline",
+      keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+      opts = { position = "right" },  -- configuration options passed to plugin
+    },
+  }
