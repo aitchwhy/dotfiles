1f9795ad402c53da0c9273f7ec5cb8177b3aa4e9
Hank Lee <hank.lee.qed@gmail.com>
2025-02-17 20:42:42 -0500

    (WIP) nvim update

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,14 @@
 return {
     {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          pyright = {},        -- Python LSP
-          tsserver = {},       -- TypeScript/JavaScript LSP
-          rust_analyzer = {},  -- Rust LSP
-          -- add others as needed, e.g. gopls for Go, clangd for C/C++, etc.
-        }
-      }
-    }
-  }
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                pyright = {}, -- Python LSP
+                tsserver = {}, -- TypeScript/JavaScript LSP
+                rust_analyzer = {}, -- Rust LSP
+                -- add others as needed, e.g. gopls for Go, clangd for C/C++, etc.
+            },
+        },
+    },
+}
+
