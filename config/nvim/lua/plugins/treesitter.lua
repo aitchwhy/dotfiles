fd89e9c8d8d6e0c7b69d0fba7ef115c5bd86635c
Hank Lee <hank.lee.qed@gmail.com>
2025-02-19 11:41:16 -0500

    (WIP) aliases + symlinks

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            -- append to the ensure_installed list
-            vim.list_extend(opts.ensure_installed, { "go", "ruby", "rust" })
-        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    {
+        "windwp/nvim-ts-autotag",
     },
 }
-
