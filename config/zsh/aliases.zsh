69ca81e01af31432c8a5ea572f7a84ab83ff190b
Hank <hank.lee.qed@gmail.com>
2025-04-18 14:01:32 -0400

    update

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -2,13 +2,11 @@
 
 local DOTFILES="${DOTFILES:-$HOME/dotfiles}"
 
-
-
 # justfile
 alias j="just"
 # alias .j='just -g'
 # alias j="just -f ~/dotfiles/user.justfile"
-for recipe in `just --justfile $DOTFILES/config/just/user.justfile --summary`; do
+for recipe in $(just --justfile $DOTFILES/config/just/user.justfile --summary); do
   alias $recipe="just --justfile '$DOTFILES/config/just/user.justfile' --working-directory . $recipe"
 done
 
@@ -103,8 +101,8 @@ alias miller='mlr'
 # alias grep='rg'
 # alias find='fd'
 # alias md='glow'
-alias ls='eza --icons' # Note: Overwrites previous ls alias
-alias ll='eza -l --icons' # Note: Overwrites previous ll alias
+alias ls='eza --icons'     # Note: Overwrites previous ls alias
+alias ll='eza -l --icons'  # Note: Overwrites previous ll alias
 alias la='eza -al --icons' # Note: Overwrites previous la alias
 # Git shortcuts
 alias gs='git status'
@@ -118,43 +116,47 @@ alias g='ghostty'
 # Homebrew aliases + shortcuts
 alias b="brew"
 #alias bdr="brew doctor"
-#alias boc="brew outdated --cask"
-#alias bof="brew outdated --formula"
 alias bupd="brew update"
 alias bupg="brew upgrade"
 alias bclean="brew cleanup --prune=all && brew autoremove"
 alias bcleanall='brew cleanup --prune=all && rm -rf $(brew --cache) && brew autoremove'
+alias bi="brew info"
 alias bin="brew install"
 alias brein="brew reinstall"
 alias bi="brew info"
 alias bs="brew search"
+alias bsa="brew search --eval-all --desc"
 alias bl="brew leaves"
 # Homebrew Cask/Bundle management
-alias bcl="brew list --cask"
 alias bcin="brew install --cask"
-alias bb="brew bundle"
-alias bbls="brew bundle dump --all --file=- --verbose"
-alias bbsave="brew bundle dump --all --verbose --global"
-alias bbcheck="brew bundle check --all --verbose --global"
+alias bb="brew bundle -g"
+alias bbe="brew bundle edit -g"
+alias bba="brew bundle add -g"
+alias bbrm="brew bundle remove -g"
+alias bbls="brew bundle dump -g --all --file=- --verbose"
+alias bbsave="brew bundle dump -g --all --verbose --global"
+alias bbcheck="brew bundle check -g --all --verbose --global"
 # Directory navigation
 alias dl='cd ~/Downloads'
 alias cf='cd ~/.config/'
 alias zcompreset="rm -f ~/.zcompdump; compinit"
 # Tailscale
 alias ts="tailscale"
+# atuin
+alias at="atuin"
+
 # Misc Shortcuts
 alias hf="huggingface-cli"
-alias lg="lazygit" # Note: Overwrites previous lg alias
+alias lg="lazygit"    # Note: Overwrites previous lg alias
 alias ld="lazydocker" # Note: Overwrites previous lg alias
 
 alias rx="repomix" # Note: Overwrites previous lg alias
 
-
 # alias jfmt="just --unstable --fmt"
 alias zj="zellij"
-alias zjl="zellij list-sessions"
-alias zja="zellij attach"
-alias zje='zellij attach "$(zellij list-sessions -n | fzf --reverse --border --no-sort --height 40% | awk '\''{print $1}'\'')"'
+alias zjls="zellij list-sessions"
+alias zja='zellij attach "$(zellij list-sessions -n | fzf --reverse --border --no-sort --height 40% | awk '\''{print $1}'\'')"'
+alias zje="zellij edit"
 
 # === End Aliases from .zshrc ===
 
@@ -180,8 +182,7 @@ alias zje='zellij attach "$(zellij list-sessions -n | fzf --reverse --border --n
 # alias fm='f man'          # Man pages
 # alias fa='f alias'        # Aliases
 # # Package management
-# alias fb='f brew'         # Brew operations
+alias fb='f brew' # Brew operations
 # alias fbi='f brew install' # Brew install
 # alias fbu='f brew uninstall' # Brew uninstall
 # === End Aliases from fzf.zsh ===
-
