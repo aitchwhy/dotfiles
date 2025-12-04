1014075d5dd39de3810ec3956d0d5acfe59d3a53
Hank Lee <hank.lee.qed@gmail.com>
2025-12-04 13:54:51 -0500

    fix(nvim): fix kulala.lua keys table structure

--- config/nvim/lua/plugins/kulala.lua
+++ config/nvim/lua/plugins/kulala.lua
@@ -1,15 +1,14 @@
+-- Kulala HTTP client for LazyVim
 return {
   {
     "mistweaverco/kulala.nvim",
+    ft = { "http", "rest" },
     keys = {
       { "<leader>Rs", desc = "Send request" },
       { "<leader>Ra", desc = "Send all requests" },
       { "<leader>Rb", desc = "Open scratchpad" },
-      desc = "Send request",
     },
-    ft = { "http", "rest" },
     opts = {
-      -- your configuration comes here
       global_keymaps = false,
       global_keymaps_prefix = "<leader>R",
       kulala_keymaps_prefix = "",
