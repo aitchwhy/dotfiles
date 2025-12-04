7d3a73cc85e03ac13be11f9ec8ae80a4a717579a
Hank Lee <hank.lee.qed@gmail.com>
2025-12-04 13:55:28 -0500

    refactor(nvim): update blink-cmp and conform headers

--- config/nvim/lua/plugins/blink-cmp.lua
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -1,6 +1,4 @@
------------------------------------------------------------------------------------
--- BLINK.CMP - MODERN COMPLETION ENGINE FOR LAZYVIM 8.X
------------------------------------------------------------------------------------
+-- Blink.cmp completion engine for LazyVim 15.x
 return {
   {
     "saghen/blink.cmp",
@@ -10,20 +8,12 @@ return {
         preset = "luasnip",
       },
       appearance = {
-        -- sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release, assuming themes add support
         use_nvim_cmp_as_default = false,
-        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
       completion = {
         accept = {
-          -- experimental auto-brackets support
-          auto_brackets = {
-            enabled = true,
-          },
+          auto_brackets = { enabled = true },
         },
         menu = {
           draw = {
@@ -38,17 +28,11 @@ return {
           enabled = vim.g.ai_cmp,
         },
       },
-
-      -- experimental signature help support
       signature = { enabled = true },
-
       sources = {
-        -- adding any nvim-cmp sources here will enable them
-        -- with blink.compat
         compat = {},
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
         providers = {
-
           copilot = {
             name = "copilot",
             module = "blink-cmp-copilot",
@@ -58,11 +42,6 @@ return {
           },
         },
       },
-
-      -- keymap = {
-      --   preset = "enter",
-      --   ["<C-y>"] = { "select_and_accept" },
-      -- },
     },
   },
   {
--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,18 +1,7 @@
------------------------------------------------------------------------------------
--- CONFORM.NVIM - CODE FORMATTING CONFIGURATION
------------------------------------------------------------------------------------
--- Modern code formatter plugin with LSP integration
--- Documentation: https://github.com/stevearc/conform.nvim
--- Features:
--- * Format-on-save functionality
--- * Multiple formatter support and chaining
--- * Formatter installation via Mason
--- * LSP integration for fallback
+-- Conform.nvim formatter configuration for LazyVim 15.x
 return {
-  -- Main formatter configuration
   {
     "stevearc/conform.nvim",
-    ---@type conform.setupOpts
     opts = {
       formatters_by_ft = {
         sh = { "shfmt" },
@@ -20,11 +9,8 @@ return {
         lua = { "stylua" },
         toml = { "taplo" },
         nix = { "nixfmt" },
-        -- Conform will run multiple formatters sequentially
         go = { "goimports", "gofmt" },
-        -- You can also customize some of the format options for the filetype
         rust = { "rustfmt", lsp_format = "fallback" },
-        -- You can use a function here to determine the formatters dynamically
         python = function(bufnr)
           if require("conform").get_formatter_info("ruff_format", bufnr).available then
             return { "ruff_format" }
@@ -32,35 +18,12 @@ return {
             return { "isort", "black" }
           end
         end,
-        -- Use the "*" filetype to run formatters on all filetypes.
         ["*"] = { "codespell" },
-        -- Use the "_" filetype to run formatters on filetypes that don't
-        -- have other formatters configured.
         ["_"] = { "trim_whitespace" },
       },
       formatters = {
         injected = { options = { ignore_errors = true } },
-        -- # Example of using dprint only when a dprint.json file is present
-        -- dprint = {
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
-        --
-        -- # Example of using shfmt with extra args
-        shfmt = {
-          prepend_args = { "-i", "2", "-ci" },
-        },
-        yq = {},
-        taplo = {},
-        nixfmt = {},
-        goimports = {},
-        gofmt = {},
-        rustfmt = {},
-        isort = {},
-        black = {},
-        stylua = {},
-        ruff = {},
+        shfmt = { prepend_args = { "-i", "2", "-ci" } },
       },
     },
   },
