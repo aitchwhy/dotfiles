23753f1d0f699b86ca465347f38392e1209f72e5
Hank Lee <hank.lee.qed@gmail.com>
2025-12-04 13:55:22 -0500

    feat(nvim): add neotest adapters for vitest, python, go

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -1,31 +1,24 @@
+-- Neotest configuration for LazyVim
 return {
-    {
-        "nvim-neotest/neotest",
-        opts = {
-            -- Can be a list of adapters like what neotest expects,
-            -- or a list of adapter names,
-            -- or a table of adapter names, mapped to adapter configs.
-            -- The adapter will then be automatically loaded with the config.
-            adapters = {},
-            -- Example for loading neotest-golang with a custom config
-            -- adapters = {
-            --   ["neotest-golang"] = {
-            --     go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
-            --     dap_go_enabled = true,
-            --   },
-            -- },
-            status = { virtual_text = true },
-            output = { open_on_run = true },
-            quickfix = {
-                open = function()
-                    if LazyVim.has("trouble.nvim") then
-                        require("trouble").open({ mode = "quickfix", focus = false })
-                    else
-                        vim.cmd("copen")
-                    end
-                end,
-            },
-
-        },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["neotest-vitest"] = {},
+        ["neotest-python"] = {},
+        ["neotest-go"] = {},
+      },
+      status = { virtual_text = true },
+      output = { open_on_run = true },
+      quickfix = {
+        open = function()
+          if LazyVim.has("trouble.nvim") then
+            require("trouble").open({ mode = "quickfix", focus = false })
+          else
+            vim.cmd("copen")
+          end
+        end,
+      },
     },
+  },
 }
