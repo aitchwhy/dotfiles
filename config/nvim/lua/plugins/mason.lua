83c5b9b06a45d054906298d0ddc943055e612e25
Hank Lee <hank.lee.qed@gmail.com>
2025-12-09 12:09:01 -0500

    fix(nvim): update mason-lspconfig.nvim org name
    Plugin was renamed from williamboman to mason-org.
    Fixes deprecation warning on startup.

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -2,7 +2,7 @@
 -- Excludes tools that are installed via Nix
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     opts = {
       automatic_installation = {
         exclude = { "nil_ls" }, -- Use nixd from Nix instead
