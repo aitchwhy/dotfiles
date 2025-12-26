592ada5dd45d11989dbb3c36cb1c72b9ef992150
Hank Lee <hank.lee.qed@gmail.com>
2025-12-26 13:10:37 -0500

    feat(nvim): add debug adapters and biome to mason.lua

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -2,6 +2,27 @@
 -- Excludes tools that are installed via Nix
 return {
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- Debug adapters (DAP)
+        "js-debug-adapter", -- TypeScript/JavaScript debugging
+        "debugpy",          -- Python debugging
+        "delve",            -- Go debugging
+        -- Note: codelldb for Rust is installed via rust extra
+
+        -- Formatters & Linters
+        "biome",            -- JS/TS/JSON formatting + linting
+
+        -- Additional linters
+        "markdownlint",     -- Markdown linting
+        "yamllint",         -- YAML linting
+        "hadolint",         -- Dockerfile linting
+        "sqlfluff",         -- SQL linting
+      },
+    },
+  },
+  {
     "mason-org/mason-lspconfig.nvim",
     opts = {
       automatic_installation = {
