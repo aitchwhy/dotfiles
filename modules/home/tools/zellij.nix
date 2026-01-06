d06111cabffa64a733cda9fe9f0f60dfbf31603d
Hank Lee <hank.lee.qed@gmail.com>
2026-01-06 13:10:29 -0500

    feat(zellij): install plugins from flake inputs

--- modules/home/tools/zellij.nix
+++ modules/home/tools/zellij.nix
@@ -2,10 +2,33 @@
 {
   config,
   lib,
+  pkgs,
+  inputs,
   ...
 }:
 let
   inherit (lib) mkEnableOption mkIf;
+  zellijConfig = ../../../config/zellij;
+
+  # Plugin sources from flake inputs
+  # zjstatus: Built WASM from flake
+  zjstatusWasm = "${inputs.zjstatus.packages.${pkgs.system}.default}/bin/zjstatus.wasm";
+
+  # room: Fetch pre-built WASM from GitHub releases
+  roomWasm = pkgs.fetchurl {
+    url = "https://github.com/rvcas/room/releases/download/v1.2.0/room.wasm";
+    sha256 = "sha256-t6GPP7OOztf6XtBgzhLF+edUU294twnu0y5uufXwrkw=";
+  };
+
+  configFiles = {
+    "zellij/config.kdl".source = "${zellijConfig}/config.kdl";
+    "zellij/layouts/default.kdl".source = "${zellijConfig}/layouts/default.kdl";
+  };
+
+  pluginFiles = {
+    "zellij/plugins/zjstatus.wasm".source = zjstatusWasm;
+    "zellij/plugins/room.wasm".source = roomWasm;
+  };
 in
 {
   options.modules.home.tools.zellij = {
@@ -19,7 +42,7 @@ in
       enableBashIntegration = false;
     };
 
-    # Zellij uses KDL format, use xdg.configFile for full config
-    xdg.configFile."zellij".source = ../../../config/zellij;
+    # Link config files and plugins individually (like yazi pattern)
+    xdg.configFile = configFiles // pluginFiles;
   };
 }
