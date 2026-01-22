05ea40aaa2bf3b2a753099bc9af07fa23299e812
Hank Lee <hank.lee.qed@gmail.com>
2026-01-22 09:38:51 -0500

    feat(nvim): add terminal mode paste mapping for wispr flow

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -21,3 +21,14 @@ map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 -- Terminal mappings
 map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 map("t", "<C-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
+-- Paste from system clipboard in terminal mode (for Wispr Flow dictation, etc.)
+map("t", "<D-v>", function()
+  local clipboard = vim.fn.getreg("+")
+  if clipboard and clipboard ~= "" then
+    local term_chan = vim.b.terminal_job_id
+    if term_chan then
+      vim.api.nvim_chan_send(term_chan, clipboard)
+    end
+  end
+end, { desc = "Paste clipboard in terminal" })
