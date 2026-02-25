91f6e7ded8fde8bc6f01dd42b9e6bb5fd9b92249
Hank Lee <hank.lee.qed@gmail.com>
2026-02-25 14:03:51 -0500

    refactor(git): consolidate manual gitconfig into home-manager SSOT

--- modules/home/tools/git.nix
+++ modules/home/tools/git.nix
@@ -70,38 +70,167 @@ in
         };
 
         alias = {
+          # Essentials
           co = "checkout";
           br = "branch";
           ci = "commit";
-          st = "status";
+          st = "status -sb";
           unstage = "reset HEAD --";
           last = "log -1 HEAD";
-          l = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+
+          # Logs
+          lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+          ll = "log --pretty=format:'%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]' --decorate --numstat";
+          lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+          lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+
+          # Workflow
+          amend = "commit --amend --no-edit";
+          fixup = "commit --fixup";
+          squash = "commit --squash";
+          undo = "reset --soft HEAD^";
+          wip = ''commit -am "WIP" --no-verify'';
+
+          # Diffs
+          df = "diff";
+          dc = "diff --cached";
+          ds = "diff --stat";
+
+          # Branch management
+          brd = "branch -d";
+          brD = "branch -D";
+          bra = "branch -a";
+          brm = "branch -m";
+
+          # Remote operations
+          pl = "pull";
+          ps = "push";
+          psu = "push -u origin HEAD";
+          psf = "push --force-with-lease";
+
+          # Stash
+          ss = "stash save";
+          sp = "stash pop";
+          sl = "stash list";
+          sa = "stash apply";
+
+          # Misc
+          contributors = "shortlog --summary --numbered";
+          find = ''!f() { git log --all --grep="$1"; }; f'';
+          aliases = "config --get-regexp alias";
         };
 
         init.defaultBranch = "main";
-        pull.rebase = true;
-        push.autoSetupRemote = true;
-        merge.conflictStyle = "zdiff3";
-        rerere.enabled = true;
-        branch.sort = "-committerdate";
-        credential.helper = "osxkeychain";
+        commit.verbose = true;
+        help.autocorrect = 1;
 
         core = {
+          editor = "nvim";
+          autocrlf = "input";
+          whitespace = "space-before-tab,-indent-with-non-tab,trailing-space";
           preloadindex = true;
           fscache = true;
           untrackedcache = true;
         };
 
+        fetch = {
+          prune = true;
+          pruneTags = true;
+          parallel = 0;
+        };
+
+        pull = {
+          rebase = true;
+          autoStash = true;
+        };
+
+        push = {
+          default = "current";
+          followTags = true;
+          autoSetupRemote = true;
+        };
+
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+          backend = "merge";
+        };
+
+        merge = {
+          conflictStyle = "zdiff3";
+          tool = "nvimdiff";
+          ff = "only";
+        };
+
         diff = {
           algorithm = "histogram";
+          renames = "copies";
           colorMoved = "default";
           colorMovedWS = "ignore-all-space";
+          tool = "nvimdiff";
+        };
+
+        status = {
+          submoduleSummary = true;
+          showUntrackedFiles = "all";
+        };
+
+        branch = {
+          autoSetupMerge = "always";
+          sort = "-committerdate";
+        };
+
+        tag.sort = "version:refname";
+
+        log = {
+          showSignature = false;
+          date = "iso";
+        };
+
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
+
+        credential.helper = "osxkeychain";
+        protocol.version = 2;
+        transfer.fsckobjects = true;
+        receive.fsckObjects = true;
+        maintenance = {
+          auto = true;
+          strategy = "incremental";
+        };
+
+        # SSH instead of HTTPS for GitHub/GitLab
+        url = {
+          "git@github.com:".insteadOf = "https://github.com/";
+          "git@gitlab.com:".insteadOf = "https://gitlab.com/";
+        };
+
+        # Color customizations
+        color = {
+          ui = "auto";
+          branch = {
+            current = "yellow reverse";
+            local = "yellow";
+            remote = "green";
+          };
+          diff = {
+            meta = "yellow bold";
+            frag = "magenta bold";
+            old = "red bold";
+            new = "green bold";
+          };
+          status = {
+            added = "green";
+            changed = "yellow";
+            untracked = "red";
+          };
         };
       };
     };
 
-    # Delta is now a separate program in home-manager
+    # Delta pager with full styling
     programs.delta = {
       enable = true;
       enableGitIntegration = true;
@@ -110,6 +239,16 @@ in
         light = false;
         line-numbers = true;
         side-by-side = true;
+        syntax-theme = "Monokai Extended";
+        plus-style = "syntax #003800";
+        minus-style = "syntax #3f0001";
+        commit-decoration-style = "bold yellow box ul";
+        file-style = "bold yellow ul";
+        file-decoration-style = "none";
+        hunk-header-decoration-style = "blue box";
+        hunk-header-file-style = "red";
+        hunk-header-line-number-style = "#067a00";
+        hunk-header-style = "file line-number syntax";
       };
     };
 
